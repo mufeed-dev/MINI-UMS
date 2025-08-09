7d33e0d7d3084fc93e426940c24536ab7b5c6f64
mufeed-dev <mufeedpv.dev@gmail.com>
2025-08-09 14:43:38 +0530

    ums-register

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/ums");
+
+const express = require("express");
+const app = express();
+
+// user routes
+const userRoute = require("./routes/userRoute");
+app.use("/", userRoute);
+app.listen(3000, () => {
+  console.log("Server is running...");
+});
