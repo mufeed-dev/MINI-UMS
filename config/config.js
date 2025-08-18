98e459edd61dd50f4159233516ac3b4b1c52711a
Mufeed Rahman P V <mufeedpv.dev@gmail.com>
2025-08-18 12:51:23 +0530

    config

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const sessionSecret = "mysitesessionsecret";
-const emailUser = "mufeed.temp@gmail.com";
-const emailPassword = "vbsp euhn hxqg jgge";
-
-module.exports = {
-  sessionSecret,
-  emailPassword,
-  emailUser,
-};
